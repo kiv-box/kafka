335eef6dbaccb057cbe6f24b1d2124ca4757a5c4
Igor Kochergin <kiv-box@users.noreply.github.com>
2017-01-09 17:35:32 +0300

    Update kafka-topic-list-generator.sh

--- deb/files/kafka-topic-list-generator.sh
+++ deb/files/kafka-topic-list-generator.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
 
-TOPIC_LIST=$(/usr/lib/kafka/bin/kafka-topics.sh --list "$@")
-TOPIC_LIST=$(echo $TOPIC_LIST | grep -v '__consumer_offsets'| grep -v 'for deletion' )
+TOPIC_LIST=$(/usr/lib/kafka/bin/kafka-topics.sh --list "${@}")
+TOPIC_LIST=$(echo "${TOPIC_LIST}" | grep -v '__consumer_offsets'| grep -v 'for deletion' )
+
 
 echo '{'
 echo '  "topics": ['
